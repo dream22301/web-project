1f7bcb8a0553765d9e796691dba3450f3327f38b
dream22301 <brilliantprincoekananda2009@gmail.com>
2026-03-02 08:47:15 +0700

    memasukkan api agar masuk ke dalam flutter

--- app/Http/Controllers/Api/AnnouncementController.php
+++ app/Http/Controllers/Api/AnnouncementController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Announcement;
 use Illuminate\Http\Request;
 
 class AnnouncementController extends Controller
 {
-    //
+    public function index() {
+        return response()->json(Announcement::latest()->get());
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,6 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AnnouncementController;
+
+Route::get('announcements', [AnnouncementController::class, 'index']);
