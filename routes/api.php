89656b808f81afe8de91058ebe6f1e4b34c38188
dream22301 <brilliantprincoekananda2009@gmail.com>
2026-03-02 21:11:18 +0700

    menambahkan api untuk testing ke flutter

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Models\Announcement;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AnnouncementController;
 
 Route::get('announcements', [AnnouncementController::class, 'index']);
+Route::get('/test', function() {
+    return Announcement::latest()->get();
+});
