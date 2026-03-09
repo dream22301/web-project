91d83529782fc0543de6e64c6a8d9ea2fbc70eab
dream22301 <brilliantprincoekananda2009@gmail.com>
2026-03-09 07:59:12 +0700

    memperbaiki jalur route

--- routes/web.php
+++ routes/web.php
@@ -24,23 +24,23 @@ Route::post('/login', [AuthController::class, 'login_process'])->name('log-in');
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 Route::get('/schedule', [ScheduleController::class, 'create']);
 
-Route::post('/schedule', [ScheduleController::class, 'create_process'])->name('schedule-create');
+Route::post('/schedule',[ScheduleController::class, 'create_process'])->name('schedule-create');
 
-Route::get('/announcement', [AnnouncementController::class, 'pengumuman']);
+Route::get('/announcement',[AnnouncementController::class, 'pengumuman']);
 
-Route::post('/announcement', [AnnouncementController::class, 'database1'])->name('announcement.database1');
-Route::delete('/announcement/{id}', [AnnouncementController::class, 'destroy'])->name('announcement.destroy');
+Route::post('/announcement',[AnnouncementController::class, 'database1'])->name('announcement.database1');
+Route::delete('/announcement/{id}',[AnnouncementController::class, 'destroy'])->name('announcement.destroy');
 
 // Question sets
-Route::get('/questions',                                        [QuestionController::class, 'index'])->name('questions.index');
-Route::post('/questions',                                       [QuestionController::class, 'store'])->name('questions.store');
-Route::get('/questions/{id}',                                   [QuestionController::class, 'show'])->name('questions.show');
-Route::post('/questions/{id}/questions',                        [QuestionController::class, 'addQuestion'])->name('questions.addQuestion');
-Route::delete('/questions/{id}/questions/{qid}',                [QuestionController::class, 'destroyQuestion'])->name('questions.destroyQuestion');
-Route::delete('/questions/{id}',                                [QuestionController::class, 'destroy'])->name('questions.destroy');
+Route::get('/questions',[QuestionController::class, 'index'])->name('questions.index');
+Route::post('/questions',[QuestionController::class, 'store'])->name('questions.store');
+Route::get('/questions/{id}',[QuestionController::class, 'show'])->name('questions.show');
+Route::post('/questions/{id}/questions',[QuestionController::class, 'addQuestion'])->name('questions.addQuestion');
+Route::delete('/questions/{id}/questions/{qid}',[QuestionController::class, 'destroyQuestion'])->name('questions.destroyQuestion');
+Route::delete('/questions/{id}',[QuestionController::class, 'destroy'])->name('questions.destroy');
 
 // Settings & Logout
-Route::get('/settings',              [SettingsController::class, 'index'])->name('settings');
-Route::post('/settings/profile',     [SettingsController::class, 'updateProfile'])->name('settings.profile');
-Route::post('/settings/password',    [SettingsController::class, 'updatePassword'])->name('settings.password');
-Route::post('/logout',               [AuthController::class, 'logout'])->name('logout');
+Route::get('/settings',[SettingsController::class, 'index'])->name('settings');
+Route::post('/settings/profile',[SettingsController::class, 'updateProfile'])->name('settings.profile');
+Route::post('/settings/password',[SettingsController::class, 'updatePassword'])->name('settings.password');
+Route::post('/logout',[AuthController::class, 'logout'])->name('logout');
