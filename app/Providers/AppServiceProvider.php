4c930008c22cc6634c9d0686d0cec7944560e5ef
dream22301 <brilliantprincoekananda2009@gmail.com>
2026-04-23 13:50:25 +0700

    penambahan sistem view exclusive untuk role

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Auth\Events\Authenticated;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,12 @@ class AppServiceProvider extends ServiceProvider
     {
         Event::listen(Authenticated::class, function ($event) {
             session()->put('last_auth_check', now());
+        });
+
+        Gate::define('view-exclusive-page', function (User $user) {
+            $pagerole = ['teacher', 'admin', 'superadmin'];
+            
+            return in_array($user->role, $pagerole);
         });
     }
 }
